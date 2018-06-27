85069ca3824244558f0643e476d096f4b4773a75
[ccym90] <[email address]>
2018-06-27 17:45:46 +0800

    added patch request

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
@@ -21,7 +22,7 @@ app.post('/todos', (req, res) => {
       res.status(400).send(e);
     });
 });
-//fina all todos and then function which takes 2 functions when err and when called and sucessful
+//find all todos and then function which takes 2 functions when err and when called and sucessful
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos})//using obj better than array - can set properties this way
@@ -59,6 +60,38 @@ app.delete('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send();
   });
+});
+
+
+//used to update items
+//body variable sub set of things user passed to us
+//update completed at property
+//then find by id and update
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
 });
 
 app.listen(port, () => {
